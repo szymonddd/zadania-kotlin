afae008f238986b1cf78f0f95d7fa2e6c7bbc964
Szymon D <szymon.dabrowski@cyfroweszkoly.pl>
2024-12-17 18:31:37 +0100

    klasa RollingTool

---
+++ app/src/main/java/rzut_orzelreszka.kt
@@ -0,0 +1,11 @@
+import kotlin.random.Random
+
+interface RollingTool {
+    fun roll(): String
+}
+
+class Coin : RollingTool {
+    override fun roll(): String {
+        return if (Random.nextBoolean()) "Heads" else "Tails"
+    }
+}
