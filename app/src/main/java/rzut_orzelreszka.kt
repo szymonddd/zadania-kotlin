0e9922fd499f8effccac3eb0c1fd567eec066e8b
Szymon D <szymon.dabrowski@cyfroweszkoly.pl>
2024-12-17 18:44:53 +0100

    fun main

--- app/src/main/java/rzut_orzelreszka.kt
+++ app/src/main/java/rzut_orzelreszka.kt
@@ -14,4 +14,31 @@ class Dice : RollingTool {
         return (1..6).random().toString()
     }
 }
+fun main() {
+    while (true) {
+        println("Choose an option:")
+        println("1. Flip a coin")
+        println("2. Roll a dice")
+        println("3. Exit")
 
+        val choice = readLine()
+        val tool: RollingTool? = when (choice) {
+            "1" -> Coin()
+            "2" -> Dice()
+            "3" -> {
+                println("Thanks for using the simulator! Goodbye!")
+                return
+            }
+            else -> {
+                println("Invalid choice, please try again.")
+                null
+            }
+        }
+
+        tool?.let {
+            println("Result: ${it.roll()}")
+        }
+
+        println()
+    }
+}
