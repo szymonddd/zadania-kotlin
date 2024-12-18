0d1af6da5bf074d8b4e1d0f521fe18e6fd02b84f
Szymon D <szymon.dabrowski@cyfroweszkoly.pl>
2024-12-19 00:27:12 +0100

    last commit

--- app/src/main/java/krokomierz.kt
+++ app/src/main/java/krokomierz.kt
@@ -6,11 +6,34 @@ class Krokomierz {
 
     fun zrobKrok() {
         liczbaKrokow++
-        println("Liczba kroków: $liczbaKrokow")
+        println("Liczba krokow: $liczbaKrokow")
     }
 
     fun resetujLicznik() {
         liczbaKrokow = 0
-        println("Licznik kroków został zresetowany.")
+        println("Licznik kroków zostal zresetowany.")
+    }
+}
+fun main() {
+    val krokomierz = Krokomierz()
+
+    while (true) {
+        println("\nCo chcesz zrobic?")
+        println("1. Zrob krok")
+        println("2. Zresetuj licznik krokow")
+        println("3. Zakoncz")
+
+        print("Wybór: ")
+        val wybor = readLine()?.toIntOrNull()
+
+        when (wybor) {
+            1 -> krokomierz.zrobKrok()
+            2 -> krokomierz.resetujLicznik()
+            3 -> {
+                println("Dziekujemy za uzywanie aplikacji!")
+                break
+            }
+            else -> println("Niepoprawny wybor, sprobuj ponownie.")
+        }
     }
 }
