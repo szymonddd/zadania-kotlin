826a2801ada414233f10be65e493f7ead5e5f5cf
Szymon D <szymon.dabrowski@cyfroweszkoly.pl>
2024-12-18 21:56:03 +0100

    fun main

--- app/src/main/java/randomquotes.kt
+++ app/src/main/java/randomquotes.kt
@@ -4,9 +4,9 @@ import kotlin.random.Random
 object Quotes {
 
     private val quotes = mutableListOf(
-        "Nie czekaj. Czas nigdy nie będzie odpowiedni. - Napoleon Hill",
-        "Wszystko, czego potrzebujesz, to już w Tobie. - Lao Tzu",
-        "Sukces to suma małych wysiłków powtarzanych dzień po dniu. - Robert Collier"
+        "Nie czekaj. Czas nigdy nie bedzie odpowiedni. - Napoleon Hill",
+        "Wszystko, czego potrzebujesz, to juz w Tobie. - Lao Tzu",
+        "Sukces to suma malych wysilkow powtarzanych dzien po dniu. - Robert Collier"
     )
 
     fun getRandomQuote(): String {
@@ -14,6 +14,14 @@ object Quotes {
     }
     fun addQuote(quote: String) {
         quotes.add(quote)
-        println("Nowy cytat został dodany do bazy.")
+        println("Nowy cytat zostal dodany do bazy.")
     }
+}
+fun main() {
+    println("Losowy cytat: ${Quotes.getRandomQuote()}")
+
+    val newQuote = "Najwieksza chwala w zyciu nie polega na tym, by nigdy nie upasc, lecz na tym, by po kazdym upadku powstac. - Nelson Mandela"
+    Quotes.addQuote(newQuote)
+
+    println("Losowy cytat po dodaniu: ${Quotes.getRandomQuote()}")
 }
